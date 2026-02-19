b7108019e89a9fdc2b358ce8019ec4723b12ae11
Andrii Tsopych <andriythoupich01@gmail.com>
2026-02-19 20:10:24 +0200

    added task implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,80 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useMemo } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+interface AutocompleteProps {
+  people: Person[];
+  onSelected: (person: Person | null) => void;
+  delay?: number;
+}
+
+const Autocomplete: React.FC<AutocompleteProps> = ({
+  people,
+  onSelected,
+  delay = 300,
+}) => {
+  const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState('');
+  const [isOpened, setIsOpened] = useState(false);
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(person =>
+      person.name.toLowerCase().includes(debouncedQuery.toLowerCase()),
+    );
+  }, [people, debouncedQuery]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, delay);
+
+    return () => {
+      window.clearTimeout(timer);
+    };
+  }, [query, delay]);
 
   return (
-    <div className="container">
-      <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
-        </h1>
+    <div className={`dropdown ${isOpened ? 'is-active' : ''}`}>
+      <div className="dropdown-trigger">
+        <input
+          type="text"
+          placeholder="Enter a part of the name"
+          className="input"
+          data-cy="search-input"
+          value={query}
+          onChange={event => {
+            onSelected(null);
+            setQuery(event.target.value);
+          }}
+          onFocus={() => setIsOpened(true)}
+          onBlur={() => setTimeout(() => setIsOpened(false), 200)}
+        />
+      </div>
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-            />
-          </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+      <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+        <div className="dropdown-content">
+          {filteredPeople.map(person => (
+            <div
+              className="dropdown-item"
+              data-cy="suggestion-item"
+              key={person.name}
+              onClick={event => {
+                setQuery(person.slug);
+                onSelected(person);
+                setIsOpened(false);
+              }}
+            >
+              <p className="has-text-link">{person.name}</p>
             </div>
-          </div>
+          ))}
         </div>
+      </div>
 
+      {debouncedQuery && filteredPeople.length === 0 && (
         <div
           className="
             notification
@@ -68,6 +88,27 @@ export const App: React.FC = () => {
         >
           <p className="has-text-danger">No matching suggestions</p>
         </div>
+      )}
+    </div>
+  );
+};
+
+export const App: React.FC = () => {
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+
+  return (
+    <div className="container">
+      <main className="section is-flex is-flex-direction-column">
+        <h1 className="title" data-cy="title">
+          {selectedPerson
+            ? `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+            : 'No selected person'}
+        </h1>
+
+        <Autocomplete
+          people={peopleFromServer}
+          onSelected={setSelectedPerson}
+        />
       </main>
     </div>
   );
